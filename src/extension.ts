89c5c2b842976b5fb6215b3e5d6d648817989d80
Matthijs Berends <m.s.berends@umcg.nl>
2025-08-03 11:52:28 +0200

    next

--- src/extension.ts
+++ src/extension.ts
@@ -8,33 +8,69 @@ export function activate(context: vscode.ExtensionContext) {
     // open up our MySQL connection in the Connections pane
     positron?.runtime.executeCode(
         "r",
-        'certedb::mysql_in_connections_pane()',
+        'certedb::mysql_init()',
         false // do not focus to Console
     );
-
-    const openFolderCmd = vscode.commands.registerCommand(
-        "certe.openFolder",
-        () => {
-            positron?.runtime.executeCode(
-                "r",
-                'certeprojects::project_open_folder()',
-                false // do not focus to Console
-            );
-        }
-    );
-    context.subscriptions.push(openFolderCmd);
 
     const moveTaskCmd = vscode.commands.registerCommand(
         "certe.moveTask",
         () => {
             positron?.runtime.executeCode(
                 "r",
-                'certeprojects::project_update()',
+                'certeprojects::positron_moveTask()',
                 false // do not focus to Console
             );
         }
     );
     context.subscriptions.push(moveTaskCmd);
+
+    const openFolderCmd = vscode.commands.registerCommand(
+        "certe.openFolder",
+        () => {
+            positron?.runtime.executeCode(
+                "r",
+                'certeprojects::positron_openFolder()',
+                false // do not focus to Console
+            );
+        }
+    );
+    context.subscriptions.push(openFolderCmd);
+
+    const copyFolderLinkCmd = vscode.commands.registerCommand(
+        "certe.copyFolderLink",
+        () => {
+            positron?.runtime.executeCode(
+                "r",
+                'certeprojects::positron_copyFolderLink()',
+                false // do not focus to Console
+            );
+        }
+    );
+    context.subscriptions.push(copyFolderLinkCmd);
+
+    const validateCmd = vscode.commands.registerCommand(
+        "certe.validate",
+        () => {
+            positron?.runtime.executeCode(
+                "r",
+                'certeprojects::positron_validate()',
+                false // do not focus to Console
+            );
+        }
+    );
+    context.subscriptions.push(validateCmd);
+
+    const openSharePointCmd = vscode.commands.registerCommand(
+        "certe.openSharePoint",
+        () => {
+            positron?.runtime.executeCode(
+                "r",
+                'certeprojects::positron_openSharePoint()',
+                false // do not focus to Console
+            );
+        }
+    );
+    context.subscriptions.push(openSharePointCmd);
 
 
     const newConsult = vscode.commands.registerCommand(
